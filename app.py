9a18300c522e508c2827134d611fe57c530077a9
sylvain <sylvain.ecuer@epita.fr>
2022-04-11 20:19:30 +0200

    set the required libs to dev and added pre commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import uvicorn
+import uvicorn  # type: ignore
+
 
 def start():
     """Launched with `poetry run start` at root level"""
-    uvicorn.run("sylvain_eric_python.main:app", host='0.0.0.0', port=80, reload=True)
+    uvicorn.run("sylvain_eric_python.main:app", host="0.0.0.0", port=80, reload=True)
